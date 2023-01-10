5ddb920b1a898acf9b5176d5865cc1afc00ac83d
Rafael Leites Luchese <rlluchese@gmail.com>
2023-01-10 19:31:01 -0300

    Styling with bulma.io

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,12 +1,38 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
   return (
     <div>
-      <h1>Header</h1>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana" />
-      <ProfileCard title="Siri" handle="@siri"/>
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>
+            Personal Digital Assistents
+          </p>
+          <p className='subtitle'>
+            What's your favorite PDA?
+          </p>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-3'>
+              <ProfileCard title="Alexa" handle="@alexa99" imagePath={AlexaImage} description="Alexa was created by Amazon." />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard title="Cortana" handle="@cortana01" imagePath={CortanaImage} description="Cortana was created by Microsoft." />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard title="Siri" handle="@siri10" imagePath={SiriImage} description="Siri was created by Apple." />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,29 @@
-function ProfileCard({ title, handle }){
+function ProfileCard({ title, handle, imagePath, description }){
     return (
-        <div>
-            <div>{title}</div>
-            <div>{handle}</div>
+        // <div>
+        //     <img src={imagePath} alt={title} />
+        //     <div>{title}</div>
+        //     <div>{handle}</div>
+        // </div>
+
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={imagePath} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6">{handle}</p>
+                    </div>
+                </div>
+                <div className="content">
+                    {description}
+                </div>
+            </div>
         </div>
     );
 }
